3b14f7fd256ce150f58163ad3efe1203b3b93f26
tennaja <108716351+tennaja@users.noreply.github.com>
2024-03-04 02:54:31 +0700

    fix secondd

--- src/app/(main-app)/device/air-compressor/components/SummaryCard.js
+++ src/app/(main-app)/device/air-compressor/components/SummaryCard.js
@@ -67,21 +67,21 @@ export default function SummaryCard() {
     },
     spanGaps: true,
     plugins: {
-      afterDraw: chart => {
-        if (chart.tooltip?._active?.length) {               
-           let x = chart.tooltip._active[0].element.x;             
-           let yAxis = chart.scales.y;
-           let ctx = chart.ctx;
-           ctx.save();
-           ctx.beginPath();
-           ctx.moveTo(x, yAxis.top);
-           ctx.lineTo(x, yAxis.bottom);
-           ctx.lineWidth = 1;
-           ctx.strokeStyle = 'rgba(0, 0, 255, 0.4)';
-           ctx.stroke();
-           ctx.restore();
-        }
-      },
+      // afterDraw: chart => {
+      //   if (chart.tooltip?._active?.length) {               
+      //      let x = chart.tooltip._active[0].element.x;             
+      //      let yAxis = chart.scales.y;
+      //      let ctx = chart.ctx;
+      //      ctx.save();
+      //      ctx.beginPath();
+      //      ctx.moveTo(x, yAxis.top);
+      //      ctx.lineTo(x, yAxis.bottom);
+      //      ctx.lineWidth = 1;
+      //      ctx.strokeStyle = 'rgba(0, 0, 255, 0.4)';
+      //      ctx.stroke();
+      //      ctx.restore();
+      //   }
+      // },
       legend: {
         position: "top"
       },
