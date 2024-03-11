bd0f041c154d70e1ff9989d0825017fce3c78246
tennaja <108716351+tennaja@users.noreply.github.com>
2024-03-11 09:40:21 +0700

    fix second change

--- src/app/(main-app)/device/Hvac-Cpms/component/ModalError.js
+++ src/app/(main-app)/device/Hvac-Cpms/component/ModalError.js
@@ -1,27 +1,27 @@
-import React from 'react'
+// import React from 'react'
 
-export default function ModalPopError() {
-  return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-            <div className="p-8 border w-auto shadow-lg rounded-md bg-white">
-              <div className="text-center">
-                <h3 className="text-2xl font-bold text-gray-900 mt-5">
-                Something Went wrong
-                </h3>
-                <div className="mt-2 px-7 py-3">
-                  <p className="text-lg text-gray-500 mt-2">`We aren't able to process your requested operation
-                  Please try again` </p>
-                </div>
-                <div className="flex justify-center mt-10 gap-5">
-                  <button
-                    className="px-4 py-2 bg-red-600 text-white font-medium rounded-md  focus:outline-none w-62"
-                    onClick={() => closeModal()}
-                  >
-                    Close
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-  )
-}
+// export default function ModalPopError() {
+//   return (
+//     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
+//             <div className="p-8 border w-auto shadow-lg rounded-md bg-white">
+//               <div className="text-center">
+//                 <h3 className="text-2xl font-bold text-gray-900 mt-5">
+//                 Something Went wrong
+//                 </h3>
+//                 <div className="mt-2 px-7 py-3">
+//                   <p className="text-lg text-gray-500 mt-2">`We aren't able to process your requested operation
+//                   Please try again` </p>
+//                 </div>
+//                 <div className="flex justify-center mt-10 gap-5">
+//                   <button
+//                     className="px-4 py-2 bg-red-600 text-white font-medium rounded-md  focus:outline-none w-62"
+//                     onClick={() => closeModal()}
+//                   >
+//                     Close
+//                   </button>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//   )
+// }
