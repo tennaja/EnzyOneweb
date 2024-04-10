7eded75fec2c4cb9d7d57968e0f3e16cd8ea46af
tennaja <108716351+tennaja@users.noreply.github.com>
2024-04-11 02:05:00 +0700

    fix issue last 10/04/2024

--- src/app/(main-app)/device/hvac/component/FloorPlan.js
+++ src/app/(main-app)/device/hvac/component/FloorPlan.js
@@ -1577,19 +1577,17 @@ export default function FloorPlan({ FloorId }) {
             <div className="p-8 border w-auto shadow-lg rounded-md bg-white">
               <h5 className="mt-5">Set Temp. (°C) : {DeviceName}</h5>
               <h5 className="mt-5">Temperature</h5>
-              <input
-                type="number"
-                placeholder="Enter your username"
-                className="border border-slate-300 rounded-md h-9 px-2 mt-2 w-80"
-                min={10}
-                max={40}
-                value={Values}
-                onChange={(e) => {
-                  onChangeValueSettemp(e.target.value);
-                  e.preventDefault();
-                }}
+              <NumericFormat 
+              type="number" 
+              className="border border-slate-300 rounded-md h-9 px-2 mt-2 w-80" 
+              min={10}
+              max={40}
+              value={Values}
+              onChange={(e) => {
+                onChangeValueSettemp(e.target.value);
+                e.preventDefault();
+              }}
               />
-
               <div className="flex justify-center mt-10 gap-5">
                 <button
                   className="px-4 py-2 bg-white text-[#14b8a6] border border-teal-300 font-medium rounded-md  focus:outline-none"
--- src/app/(main-app)/device/hvac/component/SplitTypetable.js
+++ src/app/(main-app)/device/hvac/component/SplitTypetable.js
@@ -1,9 +1,9 @@
 "use client";
-import Link from "next/link";
+
+import { NumericFormat } from 'react-number-format';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import React, { useEffect, useState } from "react";
-import { Switch } from '@headlessui/react'
 import Highlighter from "react-highlight-words";
 import {
   ChangeValueSettempSplttpye, ChangeValueSetMode, ChangeValueSetFan ,ChangeControlSplittype ,ChangeAutomationSplittype
@@ -443,18 +443,18 @@ export default function SplitTypetable(SplittypeList) {
               <h5 className="mt-5">Set Temp. (°C) : {DeviceName}</h5>
 
               <h5 className="mt-5">Temperature</h5>
-              <input
-                type="number"
-                placeholder="Enter your username"
-                className="border border-slate-300 rounded-md h-9 px-2 mt-2 w-80"
-                min={10}
-                max={40}
-                value={Values}
-                onChange={(e) => {
-                  onChangeValue(e.target.value);
-                }}
+              <NumericFormat 
+              type="number" 
+              className="border border-slate-300 rounded-md h-9 px-2 mt-2 w-80" 
+              min={10}
+              max={40}
+              value={Values} 
+              decimalScale={2}
+              onChange={(e) => {
+                onChangeValue(e.target.value);
+                e.preventDefault();
+              }}
               />
-
               <div className="flex justify-center mt-10 gap-5">
                 <button
                   className="px-4 py-2 bg-white text-[#14b8a6] border border-teal-300 font-medium rounded-md  focus:outline-none"
